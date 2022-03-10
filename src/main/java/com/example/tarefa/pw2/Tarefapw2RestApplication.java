1e26f1466c0d0239b9f8f48e4251488805c4f305
Nícolas De Conto <nicolasdeconto@hotmail.com>
2022-03-10 19:22:24 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/example/tarefa/pw2/Tarefapw2RestApplication.java
@@ -0,0 +1,11 @@
+package com.example.tarefa.pw2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class Tarefapw2RestApplication extends Application {
+}
